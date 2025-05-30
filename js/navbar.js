773fb54c72a99a461940d59ec09fe5c8a2d0ab46
Henry Moran <135266041+plobethus@users.noreply.github.com>
2025-05-30 12:02:24 -0500

    styling navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,14 @@
 // js/include.js
 document.addEventListener('DOMContentLoaded', async () => {
+  //navbar stylesheet
+  if(!document.querySelector('link[href="/css/navbar.css"]')) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = '/css/navbar.css';
+    document.head.appendChild(link);
+  }
+
+  //fetches and inserts navbar html
   try {
     const resp = await fetch('/includes/navbar.html');
     if (!resp.ok) throw new Error(`HTTP ${resp.status}`);
